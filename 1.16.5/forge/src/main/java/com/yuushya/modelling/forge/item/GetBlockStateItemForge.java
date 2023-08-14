433e03e462ed910ff4c5dde4fe4cca99cb5f06b2
XiLaiTL <1272720210@qq.com>
2023-08-15 00:46:44 +0800

    fix(1.16.5) server render item

--- 1.16.5/forge/src/main/java/com/yuushya/modelling/forge/item/GetBlockStateItemForge.java
+++ 1.16.5/forge/src/main/java/com/yuushya/modelling/forge/item/GetBlockStateItemForge.java
@@ -7,6 +7,8 @@ import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.Callable;
@@ -14,8 +16,15 @@ import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class GetBlockStateItemForge{
-    public static Callable<BlockEntityWithoutLevelRenderer> getRenderCall() {
-        return () -> new BlockEntityWithoutLevelRenderer() {
+
+    public static Callable getRenderCall(){
+        if(Dist.CLIENT.isClient()) return ()->getRender();
+        else return ()->null;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static BlockEntityWithoutLevelRenderer getRender() {
+        return new BlockEntityWithoutLevelRenderer() {
             @Override
             public void renderByItem(@NotNull ItemStack stack, ItemTransforms.@NotNull TransformType mode, @NotNull PoseStack matrices, @NotNull MultiBufferSource vertexConsumers, int light, int overlay) {
                 GetBlockStateItem.renderByItem(stack, mode, matrices, vertexConsumers, light, overlay);
