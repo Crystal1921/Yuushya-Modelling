f6199d32ab308f96555bc613a8e1266ce26b67c9
XiLaiTL <1272720210@qq.com>
2024-07-31 18:08:28 +0800

    feat(1.20.6) update to 1.20.6

---
+++ 1.20.6/common/src/main/java/com/yuushya/modelling/YuushyaClient.java
@@ -0,0 +1,36 @@
+package com.yuushya.modelling;
+
+import com.yuushya.modelling.blockentity.showblock.ShowBlockEntity;
+import com.yuushya.modelling.blockentity.showblock.ShowBlockEntityRender;
+import com.yuushya.modelling.registries.YuushyaRegistries;
+import dev.architectury.registry.client.rendering.BlockEntityRendererRegistry;
+import dev.architectury.registry.client.rendering.RenderTypeRegistry;
+import dev.architectury.registry.item.ItemPropertiesRegistry;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.List;
+
+public class YuushyaClient {
+    public static void onInitializeClient(){
+        RenderTypeRegistry.register(RenderType.cutout(), YuushyaRegistries.SHOW_BLOCK.get());
+        BlockEntityRendererRegistry.register((BlockEntityType<ShowBlockEntity>) YuushyaRegistries.SHOW_BLOCK_ENTITY.get(), ShowBlockEntityRender::new);
+
+        for (String s: List.of("rot_trans_item","pos_trans_item","micro_pos_trans_item","get_showblock_item"))
+            ItemPropertiesRegistry.register(YuushyaRegistries.ITEMS.get(s).get(),new ResourceLocation("direction"),(itemStack, clientWorld, livingEntity, i) -> ((Integer)(itemStack.getOrDefault(YuushyaRegistries.TRANS_DIRECTION.get(),0)))*0.1F );
+        ItemPropertiesRegistry.register(YuushyaRegistries.ITEMS.get("get_blockstate_item").get(),new ResourceLocation("direction"),(itemStack, clientWorld, livingEntity,i) -> {
+            BlockState blockState = itemStack.getOrDefault((DataComponentType<BlockState>) YuushyaRegistries.BLOCKSTATE.get(), Blocks.AIR.defaultBlockState());
+            if(!blockState.equals(Blocks.AIR.defaultBlockState())) return 1;
+            return 0;
+        });
+
+
+    }
+
+}
---
+++ 1.20.6/neoforge/src/main/java/com/yuushya/modelling/neoforge/YuushyaClientNeoForge.java
@@ -0,0 +1,78 @@
+package com.yuushya.modelling.neoforge;
+
+import com.yuushya.modelling.Yuushya;
+import com.yuushya.modelling.YuushyaClient;
+import com.yuushya.modelling.neoforge.client.ShowBlockModel;
+import com.yuushya.modelling.registries.YuushyaRegistries;
+import com.yuushya.modelling.utils.YuushyaUtils;
+import net.minecraft.client.renderer.block.BlockModelShaper;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.event.ModelEvent;
+import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
+
+
+@Mod(value = Yuushya.MOD_ID_USED, dist = Dist.CLIENT)
+public class YuushyaClientNeoForge {
+    public YuushyaClientNeoForge(IEventBus modBus){
+        if (FMLEnvironment.dist.isClient()) {
+            modBus.addListener(this::onInitializeClient);
+            modBus.addListener(this::onModelBaked);
+            modBus.addListener(this::handleBlockColor);
+            modBus.addListener(this::handleItemColor);
+        }
+    }
+
+    public void onInitializeClient(FMLClientSetupEvent event) {
+        YuushyaClient.onInitializeClient();
+    }
+
+    public void onModelBaked(ModelEvent.ModifyBakingResult event){
+        for(BlockState blockState: YuushyaRegistries.BLOCKS.get("showblock").get().getStateDefinition().getPossibleStates())
+            event.getModels().put(BlockModelShaper.stateToModelLocation(blockState),new ShowBlockModel());
+    }
+    /**
+     * getColor是对面片执行的，所以只需要知道这个面片事实上来自哪个方块就能知道颜色
+     * 而且原版方块的tintIndex的值除了-1之外似乎设为多少都无所谓
+     * 那么可以在生成面片时存储其来自的blockState
+     * Block类刚好可以将方块状态和id互相转换
+     * 前24位为原方块的blockState，后8位为原方块的tint（若其为正）
+     */
+    public void handleBlockColor(RegisterColorHandlersEvent.Block event) {
+        event.getBlockColors().register(
+                (state, view, pos, tintIndex) -> {
+                    if (tintIndex > -1) {
+                        // decodeTintWithState
+                        // 假设原tint为负数，则最高位为1，通常可以返回空气（因为不太可能出现上千万的方块状态），那么空气也不会被染色
+                        BlockState trueState = Block.stateById(tintIndex >> 8);
+                        int trueTint = tintIndex & 0xFF;
+                        return event.getBlockColors().getColor(trueState, view, pos, trueTint);
+                    } else {
+                        return 0xFFFFFFFF;
+                    }
+                },
+                YuushyaRegistries.SHOW_BLOCK.get()
+        );
+    }
+
+    public void handleItemColor(RegisterColorHandlersEvent.Item event) {
+        event.getItemColors().register(
+                (itemStack, i) -> {
+                    BlockState blockState = itemStack.getOrDefault((DataComponentType<BlockState>) YuushyaRegistries.BLOCKSTATE.get(), Blocks.AIR.defaultBlockState());
+                    return event.getBlockColors().getColor(blockState, null, null, i);
+                },YuushyaRegistries.ITEMS.get("get_blockstate_item").get()
+        );
+    }
+
+}
