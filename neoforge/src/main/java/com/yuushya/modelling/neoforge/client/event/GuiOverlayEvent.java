412880618549d28623e70a78d139ed167347140f
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2025-10-23 21:03:08 +0800

    GuiOverlay在客户端

--- neoforge/src/main/java/com/yuushya/modelling/neoforge/client/event/GuiOverlayEvent.java
+++ neoforge/src/main/java/com/yuushya/modelling/neoforge/client/event/GuiOverlayEvent.java
@@ -3,13 +3,14 @@ package com.yuushya.modelling.neoforge.client.event;
 import com.yuushya.modelling.Yuushya;
 import com.yuushya.modelling.neoforge.client.gui.PickColorOverlay;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 
 import static net.neoforged.neoforge.client.gui.VanillaGuiLayers.CROSSHAIR;
 
-@EventBusSubscriber
+@EventBusSubscriber(value = Dist.CLIENT)
 public class GuiOverlayEvent {
     @SubscribeEvent
     public static void registerOverlays(RegisterGuiLayersEvent event) {
