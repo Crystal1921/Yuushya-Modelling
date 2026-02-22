47b051bc403697d74ad1a78644a9e5329b51d100
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2026-02-22 22:42:09 +0800

    StackOverflow头痛医头

--- src/main/java/com/yuushya/modelling/item/showblocktool/GetBlockStateItem.java
+++ src/main/java/com/yuushya/modelling/item/showblocktool/GetBlockStateItem.java
@@ -5,6 +5,7 @@ import com.yuushya.modelling.blockentity.showblock.ShowBlock;
 import com.yuushya.modelling.blockentity.showblock.ShowBlockEntity;
 import com.yuushya.modelling.item.AbstractToolItem;
 import com.yuushya.modelling.registries.DataComponentRegistry;
+import com.yuushya.modelling.registries.ItemRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
@@ -28,11 +29,12 @@ public class GetBlockStateItem extends AbstractToolItem {
         blockState = Blocks.AIR.defaultBlockState();
     }
 
-    public static void renderByItem(ItemStack stack, ItemDisplayContext displayContext, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay) {
+    public static void renderByItem(ItemStack stack, ItemDisplayContext displayContext, PoseStack poseStack, MultiBufferSource vertexConsumers, int light, int overlay) {
+        if (stack.is(ItemRegistry.GET_BLOCKSTATE_ITEM)) return ;
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
         BlockState blockState = stack.getOrDefault(DataComponentRegistry.BLOCKSTATE.get(), Blocks.AIR.defaultBlockState());
         BlockRenderDispatcher blockRenderDispatcher = Minecraft.getInstance().getBlockRenderer();
-        itemRenderer.render(stack, displayContext, false, matrices, vertexConsumers, light, overlay, blockRenderDispatcher.getBlockModel(blockState));
+        itemRenderer.render(stack, displayContext, false, poseStack, vertexConsumers, light, overlay, blockRenderDispatcher.getBlockModel(blockState));
     }
 
     @Override
