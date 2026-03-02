33012406f1096983e24bb73d02566e6f173a0299
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2026-03-02 18:09:35 +0800

    gitignore Claude

--- src/main/java/com/yuushya/modelling/gui/engrave/EngraveItemResult.java
+++ src/main/java/com/yuushya/modelling/gui/engrave/EngraveItemResult.java
@@ -1,16 +1,18 @@
 package com.yuushya.modelling.gui.engrave;
 
+import com.yuushya.modelling.blockentity.transformData.ITransformItemDataInventory;
 import com.yuushya.modelling.blockentity.transformData.TransformItemData;
+import com.yuushya.modelling.registries.BlockEntityRegistry;
 import com.yuushya.modelling.registries.ItemRegistry;
 import com.yuushya.modelling.utils.ShareUtils;
 import lombok.Getter;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.yuushya.modelling.item.showblocktool.DestroyItem.saveToItem;
 
 public class EngraveItemResult implements IEngraveResult {
     private final ItemStack resultItemStack;
@@ -23,7 +25,13 @@ public class EngraveItemResult implements IEngraveResult {
         itemInfo.transferItems(transformDataList);
         resultItemStack = ItemRegistry.ITEM_BLOCK.get().getDefaultInstance();
         resultItemStack.setHoverName(Component.literal(name));
-        saveToItem(resultItemStack, transformDataList);
+        saveItemBlockData(resultItemStack, transformDataList);
+    }
+
+    private static void saveItemBlockData(ItemStack itemStack, List<TransformItemData> transformDataList) {
+        CompoundTag compoundTag = new CompoundTag();
+        ITransformItemDataInventory.saveAdditional(compoundTag, transformDataList);
+        BlockItem.setBlockEntityData(itemStack, BlockEntityRegistry.ITEM_BLOCK_ENTITY.get(), compoundTag);
     }
 
     public ItemStack getResultItem() {
